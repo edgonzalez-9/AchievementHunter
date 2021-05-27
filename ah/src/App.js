9eb24bc051fb403f521539e52f1711f2149967c2
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-05-26 22:49:57 -0500

    finished card component

--- ah/src/App.js
+++ ah/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import GameCard from './Components/GameCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-
+      <GameCard />
     </div>
   );
 }
--- ah/src/Components/GameCard.js
+++ ah/src/Components/GameCard.js
@@ -1,12 +1,3 @@
-{/* <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div> */}
-
 import React from 'react';
 
 function GameCard(props){
@@ -15,9 +6,11 @@ function GameCard(props){
             <img src="..." class="card-img-top" alt="..."></img>
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
-                <p class="Card-text"
+                <p class="Card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
-
         </div>
-    )
+    );
 }
+
+export default GameCard;
