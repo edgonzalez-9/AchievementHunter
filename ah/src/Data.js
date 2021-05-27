b910f44f02233e655bbfa6b608fa7b8a3ef62538
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-05-26 23:09:31 -0500

    added some info to the data page;

--- ah/src/Data.js
+++ ah/src/Data.js
@@ -2,7 +2,9 @@ let GameData =
     {
         title: 'Resident Evil 8',
         platforms: 'PC, PS4, PS5, Xbox One, Xbox Series X',
-        image: 'https://www.residentevil.com/village/assets/images/common/share.png'
+        image: 'https://www.residentevil.com/village/assets/images/common/share.png',
+        description: 'Years after the tragic events of Resident Evil 7 biohazard, Ethan Winters has started over with his wife Mia, finally living in peace and putting the past behind them. However, Chris Redfield, the legendary hero from previous Resident Evil games, suddenly disrupts their life, throwing a devastated Ethan into a new and twisted nightmare in search of answers.',
+        achievementTotal: '50'
     }
 
 export default GameData;
